900cda1518cbe4d8fb2dfd9d1c4f03e14c6574e7
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-26 12:59:38 -0300

    :sparkles: feat: video 2 finished

---
+++ test/responseMock.js
@@ -0,0 +1,19 @@
+const responseMock = [
+    {
+        "tid":5705,
+        "date":1373123005,
+        "type":"sell",
+        "price":196.52,
+        "amount":0.01
+    },
+
+    {
+        "tid":5706,
+        "date":1373124523,
+        "type":"buy",
+        "price":200,
+        "amount":0.3
+    }
+]
+
+module.exports = responseMock
